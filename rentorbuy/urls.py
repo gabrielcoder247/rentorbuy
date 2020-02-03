21b8bc56859e8b75552fe197891bd73c72f307fc
gabrielcoder <gabrielcoder247@gmail.com>
2020-02-03 07:49:22 +0400

    creating templates

--- rentorbuy/urls.py
+++ rentorbuy/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url,include
 from django.urls import path
+# from django.contrib.auth import views 
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'',include('property.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    # url(r'^logout/$', views.logout, {"next_page": '/'}), 
 
 ]
